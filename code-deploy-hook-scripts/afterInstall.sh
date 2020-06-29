a593fdd7c6029a02b44a4c3cd0cf1dcc3855fdd1
arundathipatil <patil.aru@northeastern.edu>
2020-06-28 21:22:48 -0400

    a6 changes-17

--- code-deploy-hook-scripts/afterInstall.sh
+++ code-deploy-hook-scripts/afterInstall.sh
@@ -4,4 +4,4 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo chmod +x BE-layer-1.0-SNAPSHOT.jar
 
 #Kill application if already running
-sudo kill -9 $(ps -ef|grep BE-layer-1.0 | grep -v grep | awk '{print $2}')
+kill -9 $(lsof -t -i:8080)
