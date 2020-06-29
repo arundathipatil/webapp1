af82297b00bbe00b80d13517f88a0e465099059c
arundathipatil <patil.aru@northeastern.edu>
2020-06-29 11:10:29 -0400

    a6 changes-21

--- code-deploy-hook-scripts/afterInstall.sh
+++ code-deploy-hook-scripts/afterInstall.sh
@@ -1,4 +1,10 @@
-echo "Running after install script"
+echo "This is after install script"
 cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo chmod +x BE-layer-1.0-SNAPSHOT.jar
+
+#Kill application if already running
+kill -9 $(ps -ef|grep BE-layer-1.0 | grep -v grep | awk '{print $2}')
+
+source /etc/profile.d/envvariable.sh
+nohup java -jar demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
