adbc97c7cc18219562bea762d3e0d7b26a5f7997
arundathipatil <patil.aru@northeastern.edu>
2020-06-29 13:42:14 -0400

    a6 changes-28

--- code-deploy-hook-scripts/afterInstall.sh
+++ code-deploy-hook-scripts/afterInstall.sh
@@ -1,5 +1,5 @@
 echo "This is after install script"
-cd /home/ubuntu/webapp/BE-layer/target
+cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo chmod +x BE-layer-1.0-SNAPSHOT.jar
 
