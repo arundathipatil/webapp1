d4a2598524dbb68c88c3f1394bd166120e0c9787
arundathipatil <patil.aru@northeastern.edu>
2020-07-05 19:43:29 -0400

    a7 changes

--- code-deploy-hook-scripts/afterInstall.sh
+++ code-deploy-hook-scripts/afterInstall.sh
@@ -8,3 +8,4 @@ kill -9 $(ps -ef|grep BE-layer-1.0 | grep -v grep | awk '{print $2}')
 
 source /etc/profile.d/envvariable.sh
 nohup java -jar BE-layer-1.0-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent.json -s
