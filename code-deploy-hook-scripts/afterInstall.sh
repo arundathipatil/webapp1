5fa58e560aa186d5e8f2748781d6f157cb4eafe2
arundathipatil <patil.aru@northeastern.edu>
2020-07-08 16:03:05 -0400

    a7 changes

--- code-deploy-hook-scripts/afterInstall.sh
+++ code-deploy-hook-scripts/afterInstall.sh
@@ -6,6 +6,13 @@ sudo chmod +x BE-layer-1.0-SNAPSHOT.jar
 #Kill application if already running
 kill -9 $(ps -ef|grep BE-layer-1.0 | grep -v grep | awk '{print $2}')
 
-source /etc/profile.d/envvariable.sh
-nohup java -jar BE-layer-1.0-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
+sudo cp webapp.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl stop webapp.service
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
+
+#source /etc/profile.d/envvariable.sh
+#nohup java -jar BE-layer-1.0-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent.json -s
